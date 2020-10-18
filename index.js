6f009a8e764b8f737021f4052c7fca27d807908f
mduduziavatar <mduduziavatar@gmail.com>
2020-10-18 20:50:08 +0200

    progress

--- index.js
+++ index.js
@@ -8,21 +8,17 @@ const pool = new Pool({
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser"); // add this line
 const app = express();
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false })); // add this line
-app.use(bodyParser.json()); // add  this line
-app.use(express.urlencoded({ extended: false })); // add this line
-app.use(express.json());
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-const path = require('path');
-const router = express.Router;
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false })); // add this line
+app.use(bodyParser.json());
+// const sendMail = require('./mail')
+const nodemailer = require('nodemailer');
 
-// router.get('/', function(req, res) {
+const mailGun = require('nodemailer-mailgun-transport');
 
-//     res.sendFile(path.join(__dirname, 'views', 'contacts.html'));
 
-// });
 
 
 //after you added this restart the app
@@ -32,16 +28,63 @@ app.get("/", function(req, res) {
 });
 
 app.get('/buy', function(req, res) {
-    let data = req.body.name
-    console.log('data: ', req.body.name)
+
     res.render("index");
 
-})
+});
+
+app.post('/send', function(req, res) {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Subject: ${req.body.subject}</li>
+    <li>Message: ${req.body.message}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+
+    `;
+
+    let transporter = nodemailer.createTransport({
+
+        auth: {
+            api_key: '75f013781f63757598f54a96b64398a9-2fbe671d-f60903ae',
+            domain: 'sandbox88ef036ecb064263b3fdaac595b964ee.mailgun.org'
+        },
+        tls: { rejectUnauthorized: false }
+
+    });
+
+    // send mail with defined transport object
+    const sendMail = (email, subject, text) => {
+        const mailOptions = {
+            from: email,
+            to: 'mduduziavatar@gmail.com',
+            subject,
+            text
+        };
+
+        transporter.sendMail(mailOptions, function(err, data) {
+            if (err) {
+                (err, null)
+                console.log('error occurs');
+            } else {
+                (null, data)
+                console.log('success');
+
+            }
+
+        });
+    };
+    res.render('contacts', { msg: 'Email has been sent' })
+});
 
 app.get('/contacts', function(req, res) {
     //sending messages
-    let data = req.body
-    console.log(data)
+
     res.render("contacts");
 });
 
--- mail.js
+++ mail.js
@@ -3,21 +3,28 @@ const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
     auth: {
-        api_key: '',
-        domain: ''
+        api_key: '75f013781f63757598f54a96b64398a9-2fbe671d-f60903ae',
+        domain: 'sandbox88ef036ecb064263b3fdaac595b964ee.mailgun.org'
     }
 
 };
 const transporter = nodemailer.createTransport(mailGun(auth));
+const sendMail = (email, subject, text) => {
+    const mailOptions = {
+        from: email,
+        to: 'mduduziavatar@gmail.com',
+        subject,
+        text
+    };
 
-const mailOptions = {
-    from: 'siphiweavatar@gmail.com',
-    to: 'mduduziavatar@gmail.com',
-    subject: 'Test',
-    text: 'Please work'
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+            (err, null)
+            console.log('error occurs');
+        } else {
+            (null, data)
+        }
+
+    });
 };
-transporter.sendMail(mailOptions, function(err, data) {
-    if (err) {
-        console.log('error occurs');
-    } else { message }
-});
+module.exports = sendMail;
